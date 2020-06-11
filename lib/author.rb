a1b41c8504653e4fa7b5bd9f179ae807d6f857e7
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-11 05:54:20 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
 
   attr_accessor :post, :name
+  @@post = []
 
   def initialize(name)
     @name = name
@@ -11,15 +12,17 @@ class Author
   end
 
   def add_post(post)
-
     post.author = self
+    @@post << self
   end
 
-  def add_post_by_title(name)
-
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+    @@post << self
   end
 
   def self.post_count
-
+    @@post.length
   end
 end
