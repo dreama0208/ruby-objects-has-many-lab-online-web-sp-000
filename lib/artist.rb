d74859399528373474045bf84052dd0c4d004cbc
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-11 04:15:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,10 +4,15 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
   end
 
   def songs
-    self.song.all
+    @songs
   end
 
 end
